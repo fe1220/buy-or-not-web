898b0b76d44927f2d20aeff9417ef5dfed2aea08
fe1220 <seomoon.frontend@gmail.com>
2023-08-20 00:36:31 +0900

    vanilla extract 설정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin')
+const withVanillaExtract = createVanillaExtractPlugin()
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // TODO : 쇼핑몰별 이미지 host 추가
+  images: {
+    remotePatterns: [
+      {
+        // 무신사
+        protocol: 'https',
+        hostname: 'image.msscdn.net',
+        port: '',
+        pathname: '/images/goods_img/**',
+      },
+      {
+        // 에이블리
+        protocol: 'https',
+        hostname: 'cf.product-image.s.zigzag.kr',
+        port: '',
+        pathname: '/original/c/**',
+      },
+    ],
+  },
+}
+
+module.exports = withVanillaExtract(nextConfig)
---
+++ src/app/theme.css.ts
@@ -0,0 +1,58 @@
+import { style, createTheme, createThemeContract } from '@vanilla-extract/css'
+
+export const [themeClass, vars] = createTheme({
+  color: {
+    primary: {
+      100: '#f5e8ff',
+      400: '#c16efa',
+      500: '#b757f5',
+      700: '#8e24cc',
+    },
+    gray: {
+      950: '#09090b',
+    },
+    slate: {
+      300: '#e5e8eb',
+      500: '#b0b7c1',
+      900: '#191e28',
+    },
+  },
+})
+
+export const fontTheme = createThemeContract({
+  matter: null,
+  pretendard: null,
+})
+
+export const heading = {
+  5: style({
+    fontSize: 18,
+    fontWeight: '600',
+    lineHeight: '24px',
+    letterSpacing: '-0.3px',
+  }),
+}
+
+export const subtitle = {
+  1: style({
+    fontSize: 14,
+    fontWeight: '600',
+    lineHeight: '20px',
+    letterSpacing: '-0.3px',
+  }),
+  2: style({
+    fontSize: 12,
+    fontWeight: '600',
+    lineHeight: 1.5,
+    letterSpacing: '-0.3px',
+  }),
+}
+
+export const caption = {
+  3: style({
+    fontSize: 12,
+    fontWeight: '400',
+    lineHeight: 1.5,
+    letterSpacing: '-0.3px',
+  }),
+}
